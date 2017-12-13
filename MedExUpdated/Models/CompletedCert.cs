c92ca6c2a4fa274c34ab5e7621de863530e6fc37
aspruill <alex.spruill95@gmail.com>
2017-12-13 13:21:49 -0700

    Models completed

---
+++ MedExUpdated/Models/CompletedCert.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace MedExUpdated.Models
+{
+    [Table("CompletedCert")]
+    public class CompletedCert
+    {
+        [Key]
+        public int Id { get; set; }
+        public int CertID { get; set; }
+        public int DoctorId { get; set; }
+        [DisplayName("File Upload")]
+        public HttpPostedFile FilePath { get; set; }
+        [DisplayName("Expiration Date")]
+        public DateTime ExpirationDate { get; set; }
+    }
+}
---
+++ MedExUpdated/Models/Doctor.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace MedExUpdated.Models
+{
+    [Table("Doctor")]
+    public class Doctor
+    {
+        [Key]
+        public int DoctorId { get; set; }
+        [DisplayName("First Name")]
+        public String FName { get; set; }
+        [DisplayName("Last Name")]
+        public String LName { get; set; }
+        [DisplayName("Phone")]
+        public String DocPhone { get; set; }
+        [DisplayName("Address")]
+        public String DocAddress { get; set; }
+        [DisplayName("City")]
+        public String DocCity { get; set; }
+        [DisplayName("State")]
+        public String DocState { get; set; }
+        [DisplayName("Zip")]
+        public String DocZip { get; set; }
+    }
+}
